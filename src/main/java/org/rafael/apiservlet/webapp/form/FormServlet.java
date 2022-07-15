7e41b0a5dbe3c28299b9f45bde0d1df314f5766c
TeChRF24 <f.rafael.temich24@gmail.com>
2022-07-15 02:14:03 -0500

    Finishing Form

--- src/main/java/org/rafael/apiservlet/webapp/form/FormServlet.java
+++ src/main/java/org/rafael/apiservlet/webapp/form/FormServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 @WebServlet("/registro")
 public class FormServlet extends HttpServlet {
@@ -17,6 +18,9 @@ public class FormServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         String email = req.getParameter("email");
+        String pais = req.getParameter("pais");
+        String[] lenguages = req.getParameterValues("lenguajes");
+        String[] roles = req.getParameterValues("roles");
 
         try (PrintWriter out = resp.getWriter()) {
             out.print("<!DOCTYPE html>");
@@ -32,6 +36,20 @@ public class FormServlet extends HttpServlet {
             out.print("<li> Username: " + username + "</li>");
             out.print("<li> Password: " + password + "</li>");
             out.print("<li> Email: " + email + "</li>");
+            out.print("<li> Pais: " + pais + "</li>");
+            out.print("<li>Lenguajes: <ul>");
+            Arrays.asList(lenguages).forEach(lenguage -> {
+                out.print("<li>" + lenguage + "</li>");
+            });
+            out.print("</ul></li>");
+
+            out.print("<li>Roles: <ul>");
+            Arrays.asList(roles).forEach(rol -> {
+                out.print("<li>" + rol + "</li>");
+            });
+            out.print("</ul></li>");
+
+
             out.print("</ul>");
 
             out.print("</body>");
